7566bc27a2431436e2a5e01cd923fc5bf1cfe966
KWurZ-Ow <marius.proton@gmail.com>
2023-11-25 21:34:50 +0100

    smoothing + changed stars

--- script.js
+++ script.js
@@ -16,17 +16,13 @@ camera.position.z = 5;
 camera.position.y = 1.5;
 
 //===================================================== lights
-var keyLight = new THREE.DirectionalLight(0xefefff, 3);
-keyLight.position.set(2, 2, 2)
+var keyLight = new THREE.DirectionalLight(0xefefff, 5);
+keyLight.position.set(10, 7.1, 2.6)
 scene.add(keyLight);
 
 var fillLight = new THREE.DirectionalLight(0xffefef, 0.3);
 fillLight.position.set(-2, -2, -2)
 scene.add(fillLight);
-
-const helper1 = new THREE.DirectionalLightHelper(keyLight, 1, 0xff0000);
-const helper2 = new THREE.DirectionalLightHelper(fillLight, 0.5);
-scene.add(helper1, helper2);
 
 //===================================================== resize
 window.addEventListener("resize", function () {
@@ -50,18 +46,6 @@ let isLoaded = false
 
 gltfLoader.load('./assets/Boule.glb', (gltf) => {
     scene.add(gltf.scene)
-    
-    // gltf.scene.traverse((child) => {
-    //     console.log(child)
-    //     if(child.isMesh) {
-    //         child.geometry.computeVertexNormals();
-    //         child.material = new THREE.MeshPhysicalMaterial({
-    //             clearcoat: 1,
-    //             clearcoatRoughness: 0.1,
-    //             transmission: 1,
-    //         });
-    //     }
-    // })
 
     mixer = new THREE.AnimationMixer(gltf.scene)
 
@@ -103,25 +87,15 @@ setTimeout(() => {
 }, 2500);
 
 
-//stars
-for (let i = 0; i < 700; i++) {
-    const geometry = new THREE.SphereGeometry(Math.random() * 0.1, 10, 10);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-    const star = new THREE.Mesh(geometry, material);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.listenToKeyEvents(window); // optional
 
-    // Calcul des coordonnées sphériques aléatoires pour la position de la sphère fille
-    const theta = Math.random() * Math.PI * 2; // Angle horizontal
-    const phi = Math.acos(2 * Math.random() - 1); // Angle vertical
-    const radius = 50; // Rayon de la sphère mère
-
-    // Conversion des coordonnées sphériques en coordonnées cartésiennes
-    const x = radius * Math.sin(phi) * Math.cos(theta);
-    const y = radius * Math.sin(phi) * Math.sin(theta);
-    const z = radius * Math.cos(phi);
-
-    star.position.set(x, y, z)
-    scene.add(star);
-}
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.screenSpacePanning = false;
+controls.minDistance = 1;
+controls.maxDistance = 50;
+controls.maxPolarAngle = Math.PI / 2;
 
 var clock = new THREE.Clock();
 function render() {
@@ -130,86 +104,17 @@ function render() {
     if (mixer != null) mixer.update(delta);
     // if (model) model.rotation.y += 0.010;
     stats.update();
+    controls.update();
     if (modelReady) mixer.update(clock.getDelta())
 
     renderer.render(scene, camera);
 }
 render();
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.listenToKeyEvents(window); // optional
-
-//controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
-controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-controls.dampingFactor = 0.05;
-controls.screenSpacePanning = false;
-controls.minDistance = 1;
-controls.maxDistance = 50;
-controls.maxPolarAngle = Math.PI / 2;
 
 //Interface
 const gui = new dat.GUI()
-const lightFolder = gui.addFolder('Lumières')
-const keyLightFolder = lightFolder.addFolder('Keylight')
-keyLightFolder.add(keyLight.position, 'x', -10, 10)
-keyLightFolder.add(keyLight.position, 'y', -10, 10)
-keyLightFolder.add(keyLight.position, 'z', -10, 10)
-keyLightFolder.add(keyLight, 'intensity', 0, 5)
-keyLightFolder.add(helper1, 'visible')
-const fillLightFolder = lightFolder.addFolder('FillLight')
-fillLightFolder.add(fillLight.position, 'x', -10, 10)
-fillLightFolder.add(fillLight.position, 'y', -10, 10)
-fillLightFolder.add(fillLight.position, 'z', -10, 10)
-fillLightFolder.add(fillLight, 'intensity', 0, 1)
-fillLightFolder.add(helper2, 'visible')
-lightFolder.open()
-keyLightFolder.open()
-fillLightFolder.open()
-
-//cicles
-// let bubble = document.getElementById('bubble')
-
-// let container = document.getElementById('container')
-// let circles = [
-//     {posx: 0, posy: 0, content: "moi"},
-//     {posx: 0, posy: 0, content: "bulle"},
-//     {posx: 0, posy: 0, content: "autre bulle"},
-//     {posx: 0, posy: 0, content: "ou encore une bulle beaucoup plus longue"}
-// ]
-
-// circles.forEach(circle => {
-//     let element = document.createElement('div')
-//     element.classList.add("circle")
-//     element.onmouseenter = () => {
-//         bubble.innerHTML = circle.content
-//         bubble.style.display = "block"
-//     }
-//     element.onmouseleave = () => {
-//         bubble.innerHTML = null
-//         bubble.style.display = "none"
-//     }
-//     container.appendChild(element)
-// });
-//
-// let canvas = document.getElementById('canvas');
-// let context = canvas.getContext('2d');
-
-// canvas.height = window.innerHeight;
-// canvas.width = window.innerWidth;
-// context.beginPath();
-// context.moveTo(0,0);
-// context.lineTo(1000, 800);
-// context.lineWidth = 0.5;
-
-// context.strokeStyle = '#ffffff';
-// context.stroke();
-
-// window.addEventListener('mousemove', (event) => {
-//     if (bubble.innerHTML){
-//         bubble.style.left = `${event.clientX + 15}px`
-//         bubble.style.top = `${event.clientY}px`
-//     }
-// });
+gui.add(controls, 'dampingFactor', 0.001, 0.2, 0.01)
 
 let menu = document.getElementsByClassName('menu')[0]
 let isMenuOn = false
